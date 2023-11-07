9d08a1e1c1203f049dfa3b3d686400eca090e47d
JensenKau <jkau0039@student.monash.edu>
2023-11-07 09:54:53 +0800

    yep im a little lost after this point

--- __init__.py
+++ __init__.py
@@ -1,2 +1,7 @@
 import mlmodels
 import patientdata
+
+from sklearn.svm import SVC
+
+if __name__ == "__main__":
+    print(SVC().get_params())
--- mlmodels/svm_model.py
+++ mlmodels/svm_model.py
@@ -5,6 +5,7 @@ from numpy.typing import NDArray
 import dill
 import numpy as np
 from sklearn.svm import SVC
+import sklearn.metrics as metrics
 from .base_mlmodel import BaseMLModel
 from patientdata.data_enum import PatientOutcome, PatientSex
 
@@ -35,7 +36,18 @@ class SVMModel(BaseMLModel):
         
         data_split = self.get_data_split(x_label, y_label)
         
+        output_acc = []
+        
         for split in data_split:
+            models = []
+            models_acc = []
+            test_x = [None] * len(split["test"])
+            test_y = [None] * len(split["test"])
+            
+            for i in range(len(split["test"])):
+                test_x[i] = x_label[split["test"][i]]
+                test_y[i] = y_label[split["test"][i]]
+            
             for train_val in split["train_val"]:
                 train_x = [None] * len(train_val["train"])
                 train_y = [None] * len(train_val["train"])
@@ -50,8 +62,22 @@ class SVMModel(BaseMLModel):
                     val_x[i] = x_label[train_val["val"][i]]
                     val_y[i] = y_label[train_val["val"][i]]
                     
+                curr_model = SVC(**(self.model.get_params()))
                 
-        
+                curr_model.fit(train_x, train_y)
+                
+                val_preds = curr_model.predict(val_x)
+                val_acc = metrics.accuracy_score(val_y, val_preds)
+                
+                models.append(curr_model)
+                models_acc.append(val_acc)
+                
+            best_model = models[np.argmax(models_acc)]
+            test_preds = best_model.predict(test_x)
+            
+            output_acc.append(metrics.accuracy_score(test_y, test_preds))
+            
+        return np.mean(output_acc) # TODO: not sure what to return here
     
     
     def predict_result(self, avg_fc: NDArray, std_fc: NDArray, meta: Dict[str, int | bool | float | PatientOutcome | PatientSex]) -> PatientOutcome:
