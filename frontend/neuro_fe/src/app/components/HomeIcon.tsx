00bfcf5c8364236238a94951724e33d0b0062d83
Ian Teoh <ianteohyx@gmail.com>
2024-04-03 20:19:35 +0000

    added the home button icon button

---
+++ frontend/neuro_fe/src/app/components/HomeIcon.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import IconButton from "@mui/material/IconButton";
+import HomeButton from "@mui/icons-material/Home";
+
+const HomeIcon = () => {
+  return (
+    <div>
+      <IconButton
+        aria-label="home"
+        color="primary"
+        sx={{
+          ":hover": {
+            backgroundColor: "#d1d5db",
+          },
+          margin: "20px",
+        }}
+      >
+        <HomeButton sx={{ fontSize: "50px" }} />
+      </IconButton>
+    </div>
+  );
+};
+
+export default HomeIcon;
