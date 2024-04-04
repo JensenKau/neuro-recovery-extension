c372f8e0e8c339132a864d847feb571404a348c8
Ian Teoh <ianteohyx@gmail.com>
2024-04-04 16:26:46 +0000

    added shared with me part and replace a normal add button to a button that will lead to a pop up form

--- frontend/neuro_fe/src/app/home/page.tsx
+++ frontend/neuro_fe/src/app/home/page.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import HomeIcon from "../components/HomeIcon";
 import WelcomeMessage from "../components/WelcomeMessage";
-import MyPatients from "../components/MyItems";
+import MyItems from "../components/MyItems";
+import AddIcon from "@mui/icons-material/Add";
+import CustomButton from "../components/CustomButton";
+import PatientForm from "../components/PatientForm";
 
 const page = () => {
   return (
     <div>
       <HomeIcon />
-      <WelcomeMessage name='Ian'/>
-      <MyPatients />
+      <WelcomeMessage name="Ian" />
+      <MyItems
+        initialItems={["Ian", "Jack"]}
+        FormButtonComponent={PatientForm}
+        FormButtonProps={{title: "New Patient", ButtonComponent: CustomButton, buttonProps:{children: "Add Patients", icon: AddIcon} }}
+      >
+        My Patients
+      </MyItems>
+
+      <MyItems initialItems={["Ian", "Jack"]}>Shared With Me</MyItems>
+
     </div>
   );
 };
