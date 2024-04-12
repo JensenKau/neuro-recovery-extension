ec154aa451e26bd7ab999ea5ed2baac405e9b05a
Ian Teoh <ianteohyx@gmail.com>
2024-04-12 18:23:49 +0000

    change home page structure

--- frontend/neuro_fe/src/app/home/page.tsx
+++ frontend/neuro_fe/src/app/home/page.tsx
@@ -1,26 +1,42 @@
 import React from "react";
-import HomeIcon from "../components/HomeIcon";
-import WelcomeMessage from "../components/WelcomeMessage";
 import MyItems from "../components/MyItems";
 import AddIcon from "@mui/icons-material/Add";
 import CustomButton from "../components/CustomButton";
 import PatientForm from "../components/PatientForm";
+import Folder from "@mui/icons-material/Folder";
 
 const page = () => {
   return (
     <div>
-      <HomeIcon />
-      <WelcomeMessage header="Welcome back, " name="Ian" />
+       <div className="mb-[80px] mt-[50px] ml-[30px] text-5xl">
+          Welcome Back, <span className="text-blue-600">Doctor</span>
+        </div>
       <MyItems
         initialItems={["Ian", "Jack"]}
         FormButtonComponent={PatientForm}
-        FormButtonProps={{title: "New Patient", ButtonComponent: CustomButton, buttonProps:{children: "Add Patients", icon: AddIcon} }}
+        FormButtonProps={{
+          title: "New Patient",
+          ButtonComponent: CustomButton,
+          buttonProps: { children: "Add Patients", icon: AddIcon },
+          submitFormInfo: "add patient"
+        }}
+        chipsIcon={Folder}
+        chipsHeight="55px"
+        chipsWidth="250px"
+        chipsClickable
       >
         My Patients
       </MyItems>
 
-      <MyItems initialItems={["Ian", "Jack"]}>Shared With Me</MyItems>
-
+      <MyItems
+        initialItems={["Ian", "Jack"]}
+        chipsIcon={Folder}
+        chipsHeight="55px"
+        chipsWidth="250px"
+        chipsClickable
+      >
+        Shared With Me
+      </MyItems>
     </div>
   );
 };
