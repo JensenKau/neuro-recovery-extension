f22955f75b65449b093f3cc2b789f80bc1a9af99
Ian Teoh <ianteohyx@gmail.com>
2024-04-12 18:25:03 +0000

    change dynamic pateint page structure

--- frontend/neuro_fe/src/app/home/[patient]/page.tsx
+++ frontend/neuro_fe/src/app/home/[patient]/page.tsx
@@ -1,15 +1,46 @@
-
-import React from 'react'
+import React from "react";
 import HomeIcon from "../../components/HomeIcon";
-import WelcomeMessage from "../../components/WelcomeMessage";
+import MyItems from "../../components/MyItems";
+import EditIcon from "@mui/icons-material/Edit";
+import CustomButton from "../../components/CustomButton";
+import PatientForm from "../../components/PatientForm";
 
-const page = () => {
+const page = ({ params }: any) => {
+  const patientName = decodeURIComponent(params.patient);
   return (
     <div>
-      <HomeIcon />
-      <WelcomeMessage header="Welcome back, " name="Ian" />
+      <div className="mt-[30px] ml-[30px] flex">
+        <HomeIcon />
+        <div className="mt-6 ml-5 text-5xl">
+          My Patients {">"} <span className="text-blue-600">{patientName}</span>
+        </div>
+      </div>
+      <MyItems
+        initialItems={[
+          "First Name: ",
+          "Last Name: ",
+          "Age: ",
+          "Gender: ",
+          "ROSC: ",
+          "OHCA: ",
+          "Shockable Rhythm: ",
+          "TTM: ",
+        ]}
+        FormButtonComponent={PatientForm}
+        FormButtonProps={{
+          title: "Edit Patient Infomation",
+          ButtonComponent: CustomButton,
+          buttonProps: { children: "Edit", icon: EditIcon },
+          submitFormInfo: "save changes",
+        }}
+        chipsHeight="55px"
+        chipsWidth="320px"
+        chipsClickable={false}
+      >
+        Pateint Information
+      </MyItems>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
