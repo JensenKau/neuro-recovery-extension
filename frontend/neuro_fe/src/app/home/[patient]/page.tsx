3aa42423215093d9e99f5fba9cc8a5d07e65f40e
Ian Teoh <ianteohyx@gmail.com>
2024-04-13 20:10:57 +0000

    added more components to the patient page

--- frontend/neuro_fe/src/app/home/[patient]/page.tsx
+++ frontend/neuro_fe/src/app/home/[patient]/page.tsx
@@ -4,15 +4,32 @@ import MyItems from "../../components/MyItems";
 import EditIcon from "@mui/icons-material/Edit";
 import CustomButton from "../../components/CustomButton";
 import PatientForm from "../../components/PatientForm";
+import ShareIcon from "@mui/icons-material/Share";
+import ShareForm from "../../components/ShareProfile";
 
 const page = ({ params }: any) => {
   const patientName = decodeURIComponent(params.patient);
   return (
-    <div>
-      <div className="mt-[30px] ml-[30px] flex">
-        <HomeIcon />
-        <div className="mt-6 ml-5 text-5xl">
-          My Patients {">"} <span className="text-blue-600">{patientName}</span>
+    <div className="mt-[50px] mb-[40px] ml-[50px] mr-[50px]">
+      <div className="flex justify-between mb-[50px]">
+        <div className="flex">
+          <HomeIcon />
+          <div className="mt-6 ml-5 text-5xl">
+            My Patients {">"}{" "}
+            <span className="text-blue-600">{patientName}</span>
+          </div>
+        </div>
+        <div className="mt-[30px]">
+          <ShareForm
+            title="Share Profile"
+            ButtonComponent={CustomButton}
+            buttonProps={{
+              children: "share profile",
+              icon: ShareIcon,
+              style: "text",
+            }}
+            submitFormInfo="done"
+          />
         </div>
       </div>
       <MyItems
@@ -26,16 +43,18 @@ const page = ({ params }: any) => {
           "Shockable Rhythm: ",
           "TTM: ",
         ]}
+        childrenSize="3xl"
         FormButtonComponent={PatientForm}
         FormButtonProps={{
           title: "Edit Patient Infomation",
           ButtonComponent: CustomButton,
-          buttonProps: { children: "Edit", icon: EditIcon },
+          buttonProps: { children: "Edit", icon: EditIcon, style: "outlined" },
           submitFormInfo: "save changes",
         }}
         chipsHeight="55px"
-        chipsWidth="320px"
+        chipsWidth="327px"
         chipsClickable={false}
+        chipsLinkable={false}
       >
         Pateint Information
       </MyItems>
