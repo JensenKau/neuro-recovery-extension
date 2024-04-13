a8b93734d361176025c37a2b96b53d2fa0c5d749
Ian Teoh <ianteohyx@gmail.com>
2024-04-13 20:06:59 +0000

    simplify the code in chips.tsx

--- frontend/neuro_fe/src/app/components/Chips.tsx
+++ frontend/neuro_fe/src/app/components/Chips.tsx
@@ -2,60 +2,57 @@
 import React from "react";
 import Chip from "@mui/material/Chip";
 import Link from "next/link";
+import { ClickAwayListener } from "@mui/material";
 interface Props {
   items: string[];
   icon?: React.ElementType;
   clicked?: () => void;
-  height: string
-  width: string
-  clickable: boolean
+  height: string;
+  width: string;
+  canClick: boolean;
+  linkable: boolean;
 }
 
-const Chips = ({ items, icon: Icon, clicked , height, width, clickable}: Props) => {
+const Chips = ({
+  items,
+  icon: Icon,
+  clicked,
+  height,
+  width,
+  canClick,
+  linkable,
+}: Props) => {
   return (
     <>
       {items.length === 0 && (
         <p className="flex justify-center items-center w-full">No item found</p>
       )}
 
-      <div className="flex flex-wrap gap-2.5 ml-10 mr-5">
-        {items.map((item, index) => (clickable ?
-          <Link key={item} href={`/home/${item}`}>
-            <Chip
-              key={index}
-              icon={Icon ? <Icon /> : undefined}
-              label={item}
-              onClick={clicked}
-              sx={{
-                height: {height},
-                width: {width},
-                fontSize: "1.2rem",
-                padding: "0 50px",
-                backgroundColor: "#bae6fd",
-                "&:hover": {
-                  backgroundColor: "#38bdf8",
-                },
-                margin: "5px",
-                justifyContent: "flex-start"
-              }}
-              clickable
-            />
-          </Link>:
+      <div className="flex flex-wrap gap-2.5">
+        {items.map((item, index) => (
           <Chip
-          key={index}
-          icon={Icon ? <Icon /> : undefined}
-          label={item}
-          onClick={clicked}
-          sx={{
-            height: {height},
-            width: {width},
-            fontSize: "1.2rem",
-            padding: "0 50px",
-            backgroundColor: "#bae6fd",
-            margin: "5px",
-            justifyContent: "flex-start"
-          }}
-        />
+            key={index}
+            icon={Icon ? <Icon /> : undefined}
+            label={item}
+            onClick={clicked}
+            sx={{
+              height: { height },
+              width: { width },
+              fontSize: "1.2rem",
+              padding: canClick ? "0 20px" : "0",
+              backgroundColor: "#bae6fd",
+              "&:hover": linkable
+                ? {
+                    backgroundColor: "#64b5f6",
+                  }
+                : {},
+              margin: canClick ? "5px" : "0",
+              justifyContent: canClick ? "center" : "flex-start",
+            }}
+            clickable={canClick}
+            component="a"
+            href={linkable ? `/home/${item}` : undefined}
+          />
         ))}
       </div>
     </>
