b4c9cce28922f32156335d6431c1489b1f51b897
Jensen Kau <jensenkau@gmail.com>
2024-04-16 18:19:26 +0800

    login page done

--- frontend/neuro_fe/src/app/login/page.tsx
+++ frontend/neuro_fe/src/app/login/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import React from "react";
+import { useState } from "react";
 import Button from "@mui/material/Button";
 import Stack from '@mui/material/Stack';
 import TextField from "@mui/material/TextField";
@@ -6,19 +9,48 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberme, setRememberme] = useState(localStorage.getItem("rememberme") === "true");
+
+  const submitLogin = async () => {
+    localStorage.setItem("rememberme", rememberme.toString());
+
+    const res = await fetch(
+      "http://localhost:8000/api/token/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: 
+          JSON.stringify({
+            "email": email,
+            "password": password
+          })
+      }
+    )
+
+    if (rememberme) {
+      localStorage.setItem("jwt_token", await res.text())
+    } else {
+      sessionStorage.setItem("jwt-token", await res.text())
+    }
+  }
+
   return (
     <div className="flex justify-center items-center h-screen bg-sky-100">
       <div className="w-2/5 bg-white rounded-md">
         <Stack spacing={2} className="px-10 py-16 w-full">
           <h1 className="text-center">User Login</h1>
-          <TextField label="Email" variant="outlined" />
-          <TextField type="password" label="Password" variant="outlined" />
+          <TextField label="Email" variant="outlined" onChange={(e) => setEmail(e.target.value)} />
+          <TextField type="password" label="Password" variant="outlined" onChange={(e) => setPassword(e.target.value)} />
           <div className="grid grid-cols-2">
-            <FormControlLabel control={<Checkbox />} label="Remember Me" />
-            <h1 className="text-right my-auto">Forgot Password</h1>
+            <FormControlLabel control={<Checkbox onChange={(e) => setRememberme(e.target.checked)} />} label="Remember Me" />
+            <h1 className="text-right my-auto"><u>Forgot Password</u></h1>
           </div>
-          <Button className="bg-[#1976d2]" variant="contained" color="primary">Login</Button>
-          <h1 className="text-center">New User? Sign up Now</h1>
+          <Button className="bg-[#1976d2]" variant="contained" color="primary" onClick={submitLogin}>Login</Button>
+          <h1 className="text-center">New User? <u>Sign up Now</u></h1>
         </Stack>
       </div>
     </div>
