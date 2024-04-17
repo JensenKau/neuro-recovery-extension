d714cdf4a49c3bd04a1369312a28fac1077d3f4a
JensenKau <jkau0039@student.monash.edu>
2024-04-17 18:03:38 +0000

    get patient api halfway

--- neuro_prediction/main/urls.py
+++ neuro_prediction/main/urls.py
@@ -9,8 +9,13 @@ user_urls = [
     path("create_user/", CreateUserView.as_view()),
 ]
 
+patient_urls = [
+    path("get_patients/", GetPatientsView.as_view()),
+]
+
 main_urls = [
-    path("user/", include(user_urls))
+    path("user/", include(user_urls)),
+    path("patient/", include(patient_urls)),
 ]
  
 if __name__ == "__main__":
--- neuro_prediction/main/views/patient.py
+++ neuro_prediction/main/views/patient.py
@@ -1,4 +1,25 @@
 from __future__ import annotations
 
+from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.request import Request
+from rest_framework.response import Response 
+
+from ..models import Patient, User
+
+class GetPatientsView(ListAPIView):    
+    def get_queryset(self):
+        return Patient.objects.none()
+    
+    def get(self, request: Request):
+        user_email = request.user.email
+        
+        owned = Patient.objects.filter(owner__email=user_email)
+        access = Patient.objects.filter(access__user_id=user_email)
+        
+        print(owned)
+        print(access)
+        
+        return Response()
+
 if __name__ == "__main__":
     pass
