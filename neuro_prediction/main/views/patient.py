85ce33598617f9cbc898a724cc157e51b8413ebb
JensenKau <jkau0039@student.monash.edu>
2024-04-19 18:35:25 +0000

    get patients backend api

--- neuro_prediction/main/views/patient.py
+++ neuro_prediction/main/views/patient.py
@@ -5,6 +5,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response 
 
 from ..models import Patient, User
+from ..serializers import ShortPatientSerializer, PatientSerializer
 
 class GetPatientsView(ListAPIView):    
     def get_queryset(self):
@@ -14,12 +15,15 @@ class GetPatientsView(ListAPIView):
         user_email = request.user.email
         
         owned = Patient.objects.filter(owner__email=user_email)
-        access = Patient.objects.filter(access__user_id=user_email)
+        access = Patient.objects.filter(access__email=user_email)
+                
+        owned_serializer = ShortPatientSerializer(owned, many=True)
+        access_serializer = ShortPatientSerializer(access, many=True) 
         
-        print(owned)
-        print(access)
-        
-        return Response()
+        return Response({
+            "owned": owned_serializer.data,
+            "access": access_serializer.data
+        })
 
 if __name__ == "__main__":
     pass
