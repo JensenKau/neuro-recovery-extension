391240d49251bed7eab7c2a9607ecad714e48e0d
Ian Teoh <ianteohyx@gmail.com>
2024-04-19 19:39:44 +0000

    updates home page and patient page for better design

--- frontend/neuro_fe/src/app/home/page.tsx
+++ frontend/neuro_fe/src/app/home/page.tsx
@@ -8,12 +8,12 @@ import Folder from "@mui/icons-material/Folder";
 const page = () => {
   return (
     <div className="mt-[60px] mb-[50px] ml-[50px] mr-[50px]">
-      <div className="mb-[80px] text-5xl">
+      <div className="mb-[30px] text-5xl">
         Welcome Back, <span className="text-blue-600">Doctor</span>
       </div>
+      <div className="h-px bg-blue-400 w-full mb-[50px]"></div>
       <MyItems
         initialItems={["Ian", "Jack"]}
-        childrenSize="3xl"
         FormButtonComponent={PatientForm}
         FormButtonProps={{
           title: "New Patient",
@@ -31,19 +31,22 @@ const page = () => {
         chipsClickable
         chipsLinkable
         chipsDeletable={false}
+        chipsContentCenter
+        chipsExtraPath="home"
       >
         My Patients
       </MyItems>
 
       <MyItems
         initialItems={["Ian", "Jack"]}
-        childrenSize="3xl"
         chipsIcon={Folder}
         chipsHeight="55px"
         chipsWidth="250px"
         chipsClickable
         chipsLinkable
         chipsDeletable={false}
+        chipsContentCenter
+        chipsExtraPath="home"
       >
         Shared With Me
       </MyItems>
