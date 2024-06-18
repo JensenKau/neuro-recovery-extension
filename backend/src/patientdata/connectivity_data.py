91618cb7361759bf3109b297f8f462a6b9b029b7
Jensen Kau <jensenkau@gmail.com>
2024-06-18 21:35:15 +0800

    rewrite fc code

--- backend/src/patientdata/connectivity_data.py
+++ backend/src/patientdata/connectivity_data.py
@@ -1,77 +1,57 @@
 from __future__ import annotations
-from typing import Tuple
+import warnings
+
 from numpy.typing import NDArray
+import numpy as np
+from nilearn.connectome import ConnectivityMeasure
 
 from src.patientdata.eeg_data import PatientEEGData
 
-import numpy as np
-import scipy
-import mne
-from nilearn.connectome import ConnectivityMeasure
-import warnings
-import math
 
 class PatientConnectivityData:
+    DYNAMIC_TIME_SIZE = 60
+    DYNAMIC_SHIFT_SIZE = 0.1
+    
     BRAIN_REGION = [
-        "Fp1", "Fp2", "F7", "F8", "F3", 
-        "F4", "T3", "T4", "C3", "C4", 
-        "T5", "T6", "P3", "P4", "O1", 
-        "O2", "Fz", "Cz", "Pz", "Fpz", 
+        "Fp1", "Fp2", "F7", "F8", "F3", "F4", "T3", "T4", "C3", "C4", 
+        "T5", "T6", "P3", "P4", "O1", "O2", "Fz", "Cz", "Pz", "Fpz", 
         "Oz", "F9"
     ]
     
-    def __init__(self, avg_fc: NDArray, std_fc: NDArray, static_fc: NDArray) -> None:
-        self.avg_fc = avg_fc
-        self.std_fc = std_fc
+    def __init__(self, dynamic_fc: NDArray, static_fc: NDArray) -> None:
+        self.dynamic_fc = dynamic_fc
         self.static_fc = static_fc
+        self.avg_fc = np.mean(dynamic_fc, axis=0)
+        self.std_fc = np.std(dynamic_fc, axis=0)
     
     
     @classmethod
     def load_patient_connectivity(cls, eeg_data: PatientEEGData) -> PatientConnectivityData:
-        actual_eeg = eeg_data.get_eeg_data()
-        num_points = eeg_data.get_num_points()
         sampling_frequency = eeg_data.get_sampling_frequency()
-        utility_frequency = eeg_data.get_utility_frequency()
-        regions = PatientConnectivityData.BRAIN_REGION
-        organized_data = [None] * len(regions)
-
-        for i in range(len(regions)):
-            if regions[i] in actual_eeg:
-                organized_data[i] = actual_eeg[regions[i]]
-            else:
-                organized_data[i] = np.zeros(num_points)
-                
-        organized_data, sampling_frequency = PatientConnectivityData.preprocess_data(np.array(organized_data), sampling_frequency, utility_frequency)
-        avg_fc, std_fc, static_fc = PatientConnectivityData.gnerate_fc(organized_data, sampling_frequency)
-        
-        return PatientConnectivityData(avg_fc, std_fc, static_fc)
-    
-    
-    @classmethod
-    def gnerate_fc(cls, eeg_data: NDArray, sampling_frequency: int) -> Tuple[NDArray, NDArray, NDArray]:
-        sample_size = 60
-        window_size = sample_size * sampling_frequency
-        shift_size = int(window_size * 0.1)
-        index, pointer = 0, 0
-        avg_fc, std_fc, static_fc = None, None, None
+        table = eeg_data.convert_eeg_to_table()
+        window_size = cls.DYNAMIC_TIME_SIZE * sampling_frequency
+        shift_size = int(window_size * cls.DYNAMIC_SHIFT_SIZE)
+        index = 0
+        static_fc = None
+        dynamic_fc = []
         measure = ConnectivityMeasure(kind="correlation")
-        temp_fcs = [np.zeros((len(eeg_data), len(eeg_data)), dtype=np.float64)] * int(math.ceil(len(eeg_data[0]) / shift_size))
         
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-            while index < len(eeg_data[0]):
+            static_fc = measure.fit_transform(np.array([table]).swapaxes(1, 2))[0]
+            while index < len(table[0]):
                 start = index
-                end = int(min(start + window_size, len(eeg_data[0])))
-                current_fc = measure.fit_transform(np.array([eeg_data[:, start:end]]).swapaxes(1, 2))[0]
+                end = int(min(start + window_size, len(table[0])))
+                current_fc = measure.fit_transform(np.array([table[:, start:end]]).swapaxes(1, 2))[0]
+                
                 if not np.isnan(current_fc).any():
-                    temp_fcs[pointer] = current_fc
+                    dynamic_fc.append(current_fc)
+                else:
+                    dynamic_fc.append(np.zeros((len(cls.BRAIN_REGION), len(cls.BRAIN_REGION)), dtype=np.float64))
+                    
                 index += shift_size
-                pointer += 1           
-            avg_fc = np.mean(temp_fcs, axis=0)
-            std_fc = np.std(temp_fcs, axis=0)
-            static_fc = measure.fit_transform(np.array([eeg_data]).swapaxes(1, 2))[0]
-        
-        return avg_fc, std_fc, static_fc
+
+        return PatientConnectivityData(np.asarray(dynamic_fc, dtype=np.float64), static_fc)
     
     
     def get_avg_fc(self) -> NDArray:
@@ -86,6 +66,12 @@ class PatientConnectivityData:
         return self.static_fc
     
     
+    def get_dynamic_fc(self) -> NDArray:
+        return self.dynamic_fc
+    
+    
+    def delete_dynamic_fc(self) -> None:
+        self.dynamic_fc = None
     
     
 if __name__ == "__main__":
