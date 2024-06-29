a03f0490097dfc90214e07914b73629ce7e55601
Jensen Kau <jensenkau@gmail.com>
2024-06-30 00:36:27 +0800

    nested k fold functions

--- backend/src/evaluator/nested_kfold.py
+++ backend/src/evaluator/nested_kfold.py
@@ -1,7 +1,29 @@
 from __future__ import annotations
+from typing import Dict, List
 
-class NestedKFold:
-    pass
+from sklearn.model_selection import StratifiedKFold
+
+from src.mlmodels.base_mlmodel import BaseMLModel
+from src.patientdata.patient_data import PatientData
+from src.evaluator.model_evaluator import ModelEvaluator
+
+
+class NestedKFold(ModelEvaluator):
+    def __init__(self, model: BaseMLModel) -> None:
+        super().__init__(model)
+        
+        
+    def evaluate_performance(self, dataset: List[PatientData], testset: List[PatientData] = None) -> None:
+        pass
+    
+    
+    def get_performance(self, args: str = None) -> Dict[str, Dict[str, float]]:
+        pass
+    
+    
+    def save_performance(self, file: str, add_extension: bool = True) -> None:
+        pass
+
 
 if __name__ == "__main__":
     pass
